95123f4bc440359ca85fece57305de563a078af4
calvin <Calvinkraak@hotmail.com>
2024-02-10 15:28:21 +0100

    start

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -1,5 +1,7 @@
 package Main;
 
+import entity.Player;
+
 import javax.swing.JPanel;
 import java.awt.*;
 
@@ -9,7 +11,7 @@ public class GamePanel extends JPanel implements Runnable{
     final int originalTileSize = 16; // 16x66 tile
     final int scale = 3;
 
-    final int tileSize = originalTileSize * scale;
+    public final int tileSize = originalTileSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
@@ -21,6 +23,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this,keyH);
 
     // Set players default position
     int playerX = 100;
@@ -74,20 +77,8 @@ public class GamePanel extends JPanel implements Runnable{
 
     }
     public void update()  {
-        // MOVE KEYS
-        if (keyH.upPressed == true) {
-            playerY -= playerSpeed;
-        }
-        else if(keyH.downPressed == true){
-            playerY += playerSpeed;
-        }
-        else if(keyH.leftPressed == true){
-            playerX -= playerSpeed;
-        }
-        else if(keyH.rightPressed == true){
-            playerX += playerSpeed;
-        }
 
+            player.update();
 
     }
     public void paintComponent(Graphics g) {
@@ -96,9 +87,7 @@ public class GamePanel extends JPanel implements Runnable{
 
         Graphics2D g2 = (Graphics2D)g;
 
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
+        player.draw(g2);
 
         g2.dispose();
     }
