e4bc767911161b3e19e4826ecff43ccbb4a145d2
calvin <Calvinkraak@hotmail.com>
2024-02-20 15:27:57 +0100

    start

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -21,16 +21,21 @@ public class GamePanel extends JPanel implements Runnable{
     //WORLD SETTINGS
     public final int maxWorldCol = 80;
     public final int maxWorldRow = 50;
-    public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxWorldRow;
 
     //FPS
     int FPS = 60;
+
+    //System
     TileManager tileM = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
+    Sound sound = new Sound();
     Thread gameThread;
     public CollisionChecker cChecker = new CollisionChecker(this);
+
+    // ENTITY AND OBJECT
     public Player player = new Player(this,keyH);
+
+
 
     // Set players default position
     int playerX = 100;
@@ -44,6 +49,7 @@ public class GamePanel extends JPanel implements Runnable{
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
+        playMusic(0);
     }
 
     public void startGameThread() {
@@ -100,4 +106,21 @@ public class GamePanel extends JPanel implements Runnable{
 
         g2.dispose();
     }
+    //SOUND PLAYER
+    public void playMusic(int i){
+
+        sound.setFile(i);
+        sound.play();
+        sound.loop();
+    }
+    public void stopMusic(){
+
+        sound.stop();
+    }
+    public void playSE(int i) {
+
+        sound.setFile(i);
+        sound.play();
+    }
+
 }
